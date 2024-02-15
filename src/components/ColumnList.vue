7466137733764c0f7563a2be0f97a2875acc9b26
wangchong <wangchongfire@163.com>
2024-02-15 16:45:05 +0800

    4-5

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div v-for="column in list" :key="column.id">
+            <img :src="column.avatar" :alt="column.title">
+            <h5>{{ column.title }}</h5>
+            <p>{{ column.description }}</p>
+            <a href="#">进入专栏</a>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+interface ColumnProps {
+    id: number;
+    title: string;
+    avatar: string;
+    description: string;
+}
+
+// eslint-disable-next-line no-undef
+const props = defineProps({
+    list: {
+        type: Array as PropType<ColumnProps[]>,
+        required: true
+    }
+})
+</script>
