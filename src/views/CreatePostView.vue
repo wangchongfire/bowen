528dcf9d07541fd0b7603fbee3c0db2e07622d4e
wangchong <wangchongfire@163.com>
2024-02-20 09:47:20 +0800

    9-2 上传图片文件并获取返回数据

--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
+        <input type="file" name="file" @change.prevent="handleFileChange"/>
         <ValidateForm @form-submit="handleFormSubmit">
             <template #title>
                 <div>新建文章</div>
@@ -27,6 +28,7 @@
 import ValidateInput, { IRuleProp } from '@/components/ValidateInput.vue';
 import ValidateForm from '@/components/ValidateForm.vue';
 import { ref } from 'vue';
+import axios from 'axios';
 
 const titleRule: IRuleProp[] = [
     { type: 'required', message: '文章标题不能为空' }
@@ -40,6 +42,26 @@ const detailVal = ref('');
 const handleFormSubmit = (result:boolean) => {
     console.log('发表文章校验结果：',result);
     
+}
+
+const handleFileChange = (e:Event) => {
+    const target = e.target as HTMLInputElement;
+    const files = target.files;
+    if(files){
+        const uploadedFile = files[0];
+        const formData = new FormData();
+        formData.append(uploadedFile.name,uploadedFile);
+        axios.post('/upload',formData,{
+            headers:{
+                'Content-Type': 'multipart/form-data'
+            }
+        }).then(res => {
+            console.log(res);
+        }).catch(err => {
+            console.log(err);
+            
+        })
+    }
 }
 </script>
 <style lang="scss" scoped>
